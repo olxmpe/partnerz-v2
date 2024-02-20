55760cdf4f92e78f298eb64223ac9f06018059f7
Olympe Lespagnon <olespagnon@advanced-schema.com>
2024-02-20 11:55:47 +0100

    Install and setup vue

---
+++ src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import FirstComponent from "@/components/FirstComponent.vue"
+
+</script>
+
+<template>
+    <h1>Home Page</h1>
+    <FirstComponent></FirstComponent>
+</template>
+
+<style scoped lang="scss">
+</style>
